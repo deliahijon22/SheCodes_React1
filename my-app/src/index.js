fa095c8c4a2879aaa919b37091c0b7ff1b52fedd
deliahijon22 <79318393+deliahijon22@users.noreply.github.com>
2021-04-06 16:57:53 +0200

    weather app to react

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './App.css';
+import Weather from './Weather';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <div className="app">
+      <Weather />
+    </div>
+  );
+  }
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
